82ed5f237536ed3dc28ad297d20c30481000ba63
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-11-18 11:08:11 +0900

    マイページ

---
+++ Assets/Script/CharaData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/CharaData")]
+public class CharaData : ScriptableObject
+{
+	public int chara_id;
+}
